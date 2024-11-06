830f130a2b937bc558093dde3adec93b9142cd9b
Aleksey <work.sushkevich@gmail.com>
2024-11-06 18:00:03 +0300

    task1 files upload

---
+++ task1/task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task1
+//
+//  Created by Алексей Сушкевич on 6.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ task1/task1Tests/task1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  task1Tests.swift
+//  task1Tests
+//
+//  Created by Алексей Сушкевич on 6.11.24.
+//
+
+import Testing
+@testable import task1
+
+struct task1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
